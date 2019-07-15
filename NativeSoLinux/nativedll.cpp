7884999c82d7680de0c272138dd5d05e79adf3cd
yasuhiro <yasuhiro@localhost>
2019-07-15 11:56:12 -0400

    initial

---
+++ NativeSoLinux/nativedll.cpp
@@ -0,0 +1,27 @@
+
+extern "C"
+{
+bool writeBlock(unsigned long addr, unsigned char* data, int size)
+{
+
+}
+bool writeReg32(unsigned long addr, unsigned int data)
+{
+
+}
+
+unsigned int readReg32(unsigned long addr)
+{
+
+}
+bool readBlock(unsigned long addr, unsigned char* data, int size)
+{
+
+}
+
+int waitInterrupt()
+{
+
+}
+
+}
